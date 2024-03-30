3565138e1b5145441ac0b95248fa6096787e4300
John-Paul Robinson <jpr@uab.edu>
2024-03-30 12:15:20 -0500

    Add cubify data split to data_dist to generate compression model
    The compression model is a division of the the original data set
    into smaller chunks that can be compressed individually.
    The division of data is now in 3D with the provided reshape parameter.
    Parameterized bounds and commpressor args to make script more flexible
    for caller.

--- data_dist.py
+++ data_dist.py
@@ -24,16 +24,26 @@ parser.add_argument("dataset",
                     help="dataset to compress")
 parser.add_argument("-s", "--shape", default="100x500x500",
                     help="dataset dimensions")
+parser.add_argument("-r", "--reshape", default="25x250x250",
+                    help="new dataset dimensions, must divide dataset dimensions evenly")
+parser.add_argument("-b", "--bounds", default="-6",
+                    help="commas separated list of compression bounds, 10**n")
+parser.add_argument("-c", "--compressors", default="sz,zfp",
+                    help="comma separated list of compressors to select")
 parser.add_argument("-t", "--dtype", default="float32",
                     help="dataset type")
 parser.add_argument("-j", "--json", action='store_true',
                     help="enable json output, otherwise pprint python")
 args = parser.parse_args()
 
-procs=args.nprocs
+procs=args.nprocs # derived from new shape
 dataset=args.dataset
 jsonout=args.json
 dataset_shape=[int(x) for x in args.shape.split("x")]
+dataset_newshape=[int(x) for x in args.reshape.split("x")]
+bounds=[10**int(x) for x in args.bounds.split(",")]
+compressors=[x for x in args.compressors.split(",")]
+
 if (args.dtype == "float64"):
     datatype=np.float64
 elif (args.dtype == "int32"):
@@ -41,27 +51,33 @@ elif (args.dtype == "int32"):
 else:
     datatype=np.float32
 
-# load dataset, create output path
-input_path = Path(__file__).parent / dataset
-input_data = np.fromfile(input_path, dtype=datatype).reshape(dataset_shape)
+# encoder for json to avoid int64 exceptons
+#
+# https://stackoverflow.com/a/57915246
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
 
-input_data = np.array_split(input_data, procs, axis=0)
-
-configs = [{
-        "compressor_id": compressor_id,
-        "compressor_config": {
-            "pressio:abs": bound
-        },
-        "bound": bound,
-        "idx": idx
-    } for bound, idx, compressor_id in
-        itertools.product(
-            np.logspace(-6, -5, num=1),
-            range(procs),
-            ["sz", "zfp"]
-        )
-    ]
-
+def cubify(arr, newshape):
+    '''
+    Reshape 3D shape into new 3D shape
+    
+    Credit: https://stackoverflow.com/a/42298440
+    Tutorial for reshaping: https://realpython.com/numpy-reshape/
+    '''
+    oldshape = np.array(arr.shape)
+    repeats = (oldshape / newshape).astype(int)
+    tmpshape = np.column_stack([repeats, newshape]).ravel()
+    order = np.arange(len(tmpshape))
+    order = np.concatenate([order[::2], order[1::2]])
+    # newshape must divide oldshape evenly or else ValueError will be raised
+    return arr.reshape(tmpshape).transpose(order).reshape(-1, *newshape)
 
 def run_compressor(args):
     compressor = libpressio.PressioCompressor.from_config({
@@ -78,8 +94,8 @@ def run_compressor(args):
 
     # run compressor to determine metrics
     idx=args['idx']
-    decomp_data = input_data[idx].copy()
-    comp_data = compressor.encode(input_data[idx])
+    decomp_data = args["data"][idx].copy()
+    comp_data = compressor.encode(args["data"][idx])
     decomp_data = compressor.decode(comp_data, decomp_data)
     metrics = compressor.get_metrics()
 
@@ -90,6 +106,30 @@ def run_compressor(args):
         "shape": "x".join(str(x) for x in np.shape(input_data[idx])),
         "metrics": metrics
     }
+
+# load dataset, create output path
+input_path = Path(__file__).parent / dataset
+input_data = np.fromfile(input_path, dtype=datatype).reshape(dataset_shape)
+
+input_data = cubify(input_data, dataset_newshape)
+
+configs = [{
+        "compressor_id": compressor_id,
+        "compressor_config": {
+            "pressio:abs": bound
+        },
+        "bound": bound,
+        "idx": idx,
+        "data": input_data
+    } for bound, idx, compressor_id in
+        itertools.product(
+            np.array(bounds),
+            range(input_data.shape[0]),
+            compressors
+        )
+    ]
+
+
 
 buff = dict();
 index = 0
@@ -104,6 +144,6 @@ with MPICommExecutor() as pool:
         index+=1
 
 if (jsonout):
-    print(json.dumps(buff, indent=1))
+    print(json.dumps(buff, indent=1, cls=NpEncoder))
 else:
     pprint(buff)
