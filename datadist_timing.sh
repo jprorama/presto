241c752c0d4c0efc7ddc294d6c9890e9292a35a6
John-Paul Robinson <jpr@uab.edu>
2024-07-04 11:22:38 +0000

    Change jobsubmit to use custom hdf5 file rather than directory
    The use of directory changed too many of the locations of the
    test outputs and messed with the path assumptions of the submit
    script.
    
    Change to explicitly specifying the hdf5 output file in the
    default storage location.
    Use jobid in output file to keep the names distinct.

--- datadist_timing.sh
+++ datadist_timing.sh
@@ -8,7 +8,7 @@ RUNS=$3
 BATCH_TAG=$4
 H5GEN_ARGS=$5
 
-HDF5_FILE=storage/${PBS_JOBID}/test5-${DATASET}-${RANKS}.h5
+HDF5_FILE=test-${PBS_JOBID}.h5
 
 . ~/bin/set_nuio
 
@@ -20,7 +20,7 @@ function run_bench {
 }
 
 function clean_up {
-  rm -f $HDF5_FILE
+  rm -f storage/$HDF5_FILE
 }
 
 #expdir=$1
@@ -34,8 +34,8 @@ do
 exp_name="uniform_sync_${DATASET}+n${RANKS}+r$i"
 echo -n ${exp_name}": "
 h5bench_script=uniform-${DATASET}-${RANKS}-h5b.json
-./h5gen.py --directory "storage/_${PBS_JOBID}/"  $H5GEN_ARGS ${h5bench_script}-template > ${h5bench_script}_${PBS_JOBID}
-result="$(run_bench h5bench --debug ${h5bench_script}.${PBS_JOBID})"
+./h5gen.py --hdf5file $HDF5_FILE  $H5GEN_ARGS ${h5bench_script}-template > ${h5bench_script}_${PBS_JOBID}
+result="$(run_bench h5bench --debug ${h5bench_script}_${PBS_JOBID})"
 echo ${exp_name} > ${result}/experiment
 mv ${result} ${expdir}
 clean_up
@@ -43,7 +43,7 @@ clean_up
 exp_name="normal_sync_${DATASET}+n${RANKS}+stdev0+r$i"
 echo -n ${exp_name}": "
 h5bench_script=normal-${DATASET}-${RANKS}-h5b.json
-./h5gen.py --directory "storage/_${PBS_JOBID}/"  $H5GEN_ARGS ${h5bench_script}-template > ${h5bench_script}_${PBS_JOBID}
+./h5gen.py --hdf5file $HDF5_FILE  $H5GEN_ARGS ${h5bench_script}-template > ${h5bench_script}_${PBS_JOBID}
 result="$(run_bench h5bench --debug ${h5bench_script}_${PBS_JOBID})"
 echo ${exp_name} > ${result}/experiment
 mv ${result} ${expdir}
@@ -52,7 +52,7 @@ clean_up
 exp_name="datadist_sync_${DATASET}+n${RANKS}+interp+r$i"
 echo -n ${exp_name}": "
 h5bench_script=datadist-${DATASET}-${RANKS}-h5b.json
-./h5gen.py --directory "storage/_${PBS_JOBID}/"  $H5GEN_ARGS ${h5bench_script}-template > ${h5bench_script}_${PBS_JOBID}
+./h5gen.py --hdf5file $HDF5_FILE  $H5GEN_ARGS ${h5bench_script}-template > ${h5bench_script}_${PBS_JOBID}
 result="$(run_bench h5bench --debug ${h5bench_script}_${PBS_JOBID})"
 echo ${exp_name} > ${result}/experiment
 mv ${result} ${expdir}
@@ -61,7 +61,7 @@ clean_up
 exp_name="datadist_sync_${DATASET}+n${RANKS}+mean+r$i"
 echo -n ${exp_name}": "
 h5bench_script=datadist-${DATASET}-mean-${RANKS}-h5b.json
-./h5gen.py --directory "storage/_${PBS_JOBID}/"  $H5GEN_ARGS ${h5bench_script}-template > ${h5bench_script}_${PBS_JOBID}
+./h5gen.py --hdf5file $HDF5_FILE  $H5GEN_ARGS ${h5bench_script}-template > ${h5bench_script}_${PBS_JOBID}
 result="$(run_bench h5bench --debug ${h5bench_script}_${PBS_JOBID})"
 echo ${exp_name} > ${result}/experiment
 mv ${result} ${expdir}
