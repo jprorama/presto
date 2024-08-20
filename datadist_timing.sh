2ffbf14c30d70f6a9968746bb8381d781b9c9f3c
John-Paul Robinson <jpr@uab.edu>
2024-08-20 03:52:06 +0000

    Simplify test invocation from script and job submit
    Encapsulate the test run function and replace test conditionals
    with calls to test runner making it easier to request test
    combinations at runtime.
    
    Add Darshan summary to job results dir
    Add h5bench config script to job results dir
    
    Add MPIIO hints write out.

--- datadist_timing.sh
+++ datadist_timing.sh
@@ -8,6 +8,9 @@ then
   set -x
 fi
 
+# show the collective hits active at file open
+export MPICH_MPIIO_HINTS_DISPLAY=1
+
 DATASET=${DATASET:-$1}
 RANKS=${RANKS:-$2}
 RUNS=${RUNS:-$3}
@@ -22,13 +25,33 @@ HDF5_FILE=test-${PBS_JOBID}.h5
 
 cd ~/projects/p2
 
+# wrap h5bench call and select run artifact dir
 function run_bench {
   echo $@ 1>&2
   $@ 2>&1 |  awk '/SUCCESS/{print $14}' | sed -e 's/)//'
 }
 
+# remove target hdf5 file used by tests
 function clean_up {
   rm -f storage/$HDF5_FILE
+}
+
+# abstract all steps to support an experimental run
+function run_exp {
+  echo -n ${exp_name}": "
+  # set up h5bench config
+  h5b_json=${h5bench_script}_${PBS_JOBID}.json
+  #h5b_log=${h5bench_script}_${PBS_JOBID}-h5bench.log
+  ./h5gen.py --hdf5file $HDF5_FILE --ranks $RANKS $H5GEN_ARGS ${h5bench_script}.json-template > $h5b_json
+  # run h5bench
+  result="$(run_bench h5bench --debug $h5b_json)"
+  # collect artifacts
+  echo ${exp_name} > ${result}/experiment
+  #cp -u $h5b_log ${expdir}
+  mv --backup=numbered $h5b_json ${expdir}
+  mv ${result} ${expdir}
+  # reset HDF5 write target
+  clean_up
 }
 
 #expdir=$1
@@ -39,69 +62,49 @@ clean_up
 for (( i=0; i<${RUNS}; i=$i+1 ))
 do
 
-if [[ $TESTS =~ "uniform" ]]
-then
-exp_name="uniform_sync_${DATASET}+n${RANKS}+r$i"
-echo -n ${exp_name}": "
-h5bench_script=uniform-${DATASET}-h5b
-h5b_json=${h5bench_script}_${PBS_JOBID}.json
-h5b_log=${h5bench_script}_${PBS_JOBID}-h5bench.log
-./h5gen.py --hdf5file $HDF5_FILE  $H5GEN_ARGS ${h5bench_script}.json-template > $h5b_json
-result="$(run_bench h5bench --debug $h5b_json)"
-echo ${exp_name} > ${result}/experiment
-cp -u $h5b_log ${expdir}
-mv --backup=numbered $h5b_json ${expdir}
-mv ${result} ${expdir}
-clean_up
-fi
+  if [[ $TESTS =~ "uniform" ]]
+  then
+    exp_name="uniform_sync_${DATASET}+n${RANKS}+r$i"
+    h5bench_script=uniform-${DATASET}-h5b
+    run_exp
+  fi
 
-if [[ $TESTS =~ "normal" ]]
-then
-exp_name="normal_sync_${DATASET}+n${RANKS}+stdev0+r$i"
-echo -n ${exp_name}": "
-h5bench_script=normal-${DATASET}-h5b
-h5b_json=${h5bench_script}_${PBS_JOBID}.json
-h5b_log=${h5bench_script}_${PBS_JOBID}-h5bench.log
-./h5gen.py --hdf5file $HDF5_FILE  $H5GEN_ARGS ${h5bench_script}.json-template > $h5b_json
-result="$(run_bench h5bench --debug $h5b_json)"
-echo ${exp_name} > ${result}/experiment
-cp -u $h5b_log ${expdir}
-mv --backup=numbered $h5b_json ${expdir}
-mv ${result} ${expdir}
-clean_up
-fi
+  if [[ $TESTS =~ "normal" ]]
+  then
+    exp_name="normal_sync_${DATASET}+n${RANKS}+stdev0+r$i"
+    h5bench_script=normal-${DATASET}-h5b
+    run_exp
+  fi
 
-if [[ $TESTS =~ "datadist" ]]
-then
-exp_name="datadist_sync_${DATASET}+n${RANKS}+interp+r$i"
-echo -n ${exp_name}": "
-h5bench_script=datadist-${DATASET}-h5b
-h5b_json=${h5bench_script}_${PBS_JOBID}.json
-h5b_log=${h5bench_script}_${PBS_JOBID}-h5bench.log
-./h5gen.py --hdf5file $HDF5_FILE  $H5GEN_ARGS ${h5bench_script}.json-template > $h5b_json
-result="$(run_bench h5bench --debug $h5b_json)"
-echo ${exp_name} > ${result}/experiment
-cp -u $h5b_log ${expdir}
-mv --backup=numbered $h5b_json ${expdir}
-mv ${result} ${expdir}
-clean_up
-fi
+  if [[ $TESTS =~ "datadist" ]]
+  then
+    exp_name="datadist_sync_${DATASET}+n${RANKS}+interp+r$i"
+    h5bench_script=datadist-${DATASET}-h5b
+    # specify data distribution
+    H5GEN_ARGS="$H5GEN_ARGS --datadist $DATASET-interp-$RANKS.dist"
+    run_exp
+  fi
 
-if [[ $TESTS =~ "mean" ]]
-then
-exp_name="datadist_sync_${DATASET}+n${RANKS}+mean+r$i"
-echo -n ${exp_name}": "
-h5bench_script=datadist-${DATASET}-mean-h5b
-h5b_json=${h5bench_script}_${PBS_JOBID}.json
-h5b_log=${h5bench_script}_${PBS_JOBID}-h5bench.log
-./h5gen.py --hdf5file $HDF5_FILE  $H5GEN_ARGS ${h5bench_script}.json-template > $h5b_json
-result="$(run_bench h5bench --debug $h5b_json)"
-echo ${exp_name} > ${result}/experiment
-cp -u $h5b_log ${expdir}
-mv --backup=numbered $h5b_json ${expdir}
-mv ${result} ${expdir}
-clean_up
-fi
+  if [[ $TESTS =~ "mean" ]]
+  then
+    exp_name="datadist_sync_${DATASET}+n${RANKS}+mean+r$i"
+    h5bench_script=datadist-${DATASET}-mean-h5b
+    # specify data distribution
+    H5GEN_ARGS="$H5GEN_ARGS --datadist $DATASET-mean-$RANKS.dist"
+    run_exp
+  fi
 
 done
 
+# gather up darshan logs for h5benchmarks
+darshanlog=`darshan-config --log-path`
+jobid=`echo $PBS_JOBID | cut -d. -f1`
+mkdir -p ${expdir}/darshan/
+cp -up   ${darshanlog}/2024/*/*/*id${jobid}* \
+	 ${expdir}/darshan/
+
+# gathe h5bench logs for run
+for file in *_${PBS_JOBID}-h5bench.log
+do
+  mv --backup=numbered $file ${expdir}
+done
